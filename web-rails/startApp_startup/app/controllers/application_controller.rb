7de7f6f6162678ad36dd5a2a39e457ad924b7be0
Leandro Rodriguez <leandro.rodriguezg@gmail.com>
2016-06-26 02:37:14 -0300

    Agrego bootstrap. Hago layout base

--- web-rails/startApp_startup/app/controllers/application_controller.rb
+++ web-rails/startApp_startup/app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  #Use particular layout, not default
+  layout "application_styled"
 end
