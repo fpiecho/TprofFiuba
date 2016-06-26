a7191cead865d9a5aab414b7ab8bbe74c6b8b785
Leandro Rodriguez <leandro.rodriguezg@gmail.com>
2016-06-26 02:07:38 -0300

    Se agrega devise para autenticacion de usuarios, con las vistas y controladores basicos (sin estilos)

--- web-rails/startApp_startup/config/routes.rb
+++ web-rails/startApp_startup/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+  
+
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
