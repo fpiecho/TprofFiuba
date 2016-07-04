086e175376e18cdb690f8695e6b834710d859337
Leandro Rodriguez <leandro.rodriguezg@gmail.com>
2016-07-03 21:35:01 -0300

    Agrego validación de usuario al controlador de mobile_apps

--- web-rails/startApp_startup/app/controllers/mobile_apps_controller.rb
+++ web-rails/startApp_startup/app/controllers/mobile_apps_controller.rb
@@ -1,4 +1,5 @@
 class MobileAppsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_mobile_app, only: [:show, :edit, :update, :destroy]
 
   # GET /mobile_apps
