4e69838dd5722fee82a85e9bb8aa964363101332
Leandro Rodriguez <leandro.rodriguezg@gmail.com>
2016-07-24 03:10:07 -0300

    Agrego opción de cambio de contraseña. Emprolijamiento de algunas otras cosas

--- web-rails/startApp_startup/app/controllers/welcome_controller.rb
+++ web-rails/startApp_startup/app/controllers/welcome_controller.rb
@@ -2,6 +2,8 @@ class WelcomeController < ApplicationController
   def index
   	if !user_signed_in?
   		redirect_to new_user_session_path
+  	else
+  		redirect_to mobile_apps_path
   	end
   end
 end
