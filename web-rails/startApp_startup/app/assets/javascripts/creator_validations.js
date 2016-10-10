b7970be7c36d7e9053f501b9d492525dbff88d28
Leandro Rodriguez <leandro.rodriguezg@gmail.com>
2016-10-10 15:06:32 -0300

    Se emprolija el estilo de la edicion de estilos. Se muestran mensajes de error al validar los campos

--- web-rails/startApp_startup/app/assets/javascripts/creator_validations.js
+++ web-rails/startApp_startup/app/assets/javascripts/creator_validations.js
@@ -2,6 +2,12 @@
 //Aviso: no parece tomar el \d para los digitos. Usar [0-9]
 
 var elementsValidations = {
-	"background-color": "^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$", 
-	"height": "^([0-9]{1,2}|100)%$"
+	"background-color": {
+		"validation": "^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$",
+		"errorMessage": "Color de fondo inválido"
+	}, 
+	"height": {
+		"validation": "^([0-9]{1,2}|100)%$",
+		"errorMessage": "Altura inválida"
+	}
 };
