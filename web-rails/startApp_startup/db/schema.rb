b2cae78c4e4f998f6de3161e1ecc40fab5b5108e
Leandro Rodriguez <leandro.rodriguezg@gmail.com>
2017-01-06 23:03:11 -0300

    Y una cagada mas

--- web-rails/startApp_startup/db/schema.rb
+++ web-rails/startApp_startup/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160911030300) do
+ActiveRecord::Schema.define(version: 20161225235118) do
 
   create_table "mobile_app_screens", force: :cascade do |t|
     t.string   "mobile_app"
@@ -56,5 +56,12 @@ ActiveRecord::Schema.define(version: 20160911030300) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "versions", force: :cascade do |t|
+    t.string   "description"
+    t.integer  "mobile_app_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
 end
